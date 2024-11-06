c43d10ffea02bb1cb9ae00a98c29363a6bca7b21
henrychris <64085210+henrychris@users.noreply.github.com>
2024-11-06 13:52:06 +0100

    Create asyncHandler.ts

---
+++ src/common/util/asyncHandler.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+// Utility to wrap async functions and catch errors
+export const asyncHandler = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<any>,
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+};
